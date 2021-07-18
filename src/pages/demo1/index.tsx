6e52e17934cdd1e10201ecbe04d4c495a6437737
hill.xu <13856583506@163.com>
2021-07-19 07:32:46 +0800

    鼠标点击绘制webgl上的点

--- src/pages/demo1/index.tsx
+++ src/pages/demo1/index.tsx
@@ -3,19 +3,22 @@ import { Color } from 'three'
 import { initShaders } from '../../utils/shader'
 import './index.css'
 interface initState {
-  webGlIns: any
+  gl: any
   color: any
+  canvas: any
   vertsxShader: string
   fragmentShader: string
 }
 class Demo1 extends React.Component {
   state: initState = {
-    webGlIns: null,
+    gl: null,
     color: null,
+    canvas: null,
     vertsxShader: `
+     attribute vec4 a_position;
      void main() {
-       gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-       gl_PointSize = 100.0;
+       gl_Position = a_position;
+       gl_PointSize = 10.0;
      }
     `, // 顶点着色器
     fragmentShader: `
@@ -26,16 +29,17 @@ class Demo1 extends React.Component {
   }
   initWebGl = (cb?: Function[]) => {
     const canvas: any = document.getElementById('canvas')
-    canvas.width = 500
-    canvas.height = 500
-    const webGlIns = canvas.getContext('webgl')
+    canvas.width = 1000
+    canvas.height = 1000
+    const gl = canvas.getContext('webgl')
     const color = new Color("rgba(255, 0, 0, 1)")
     const { r, g, b } = color
-    webGlIns.clearColor(r, g, b, 1)
-    webGlIns.clear(webGlIns.COLOR_BUFFER_BIT)
+    gl.clearColor(r, g, b, 1)
+    gl.clear(gl.COLOR_BUFFER_BIT)
     this.setState({
-      webGlIns,
-      color
+      gl,
+      color,
+      canvas
     }, () => {
       Array.isArray(cb) && cb.forEach((fn: Function) => {
         typeof fn === 'function' && fn()
@@ -43,27 +47,49 @@ class Demo1 extends React.Component {
     })
   }
   chnageCanvasColor = () => {
-    const { webGlIns, color } = this.state
+    const { gl, color } = this.state
     color.offsetHSL(0.005, 0, 0) //  色相 饱和的 亮度的偏移
-    webGlIns.clearColor(color.r, color.g, color.b, 1)
-    webGlIns.clear(webGlIns.COLOR_BUFFER_BIT)
+    gl.clearColor(color.r, color.g, color.b, 1)
+    gl.clear(gl.COLOR_BUFFER_BIT)
     requestAnimationFrame(this.chnageCanvasColor)
   }
   drawPoints = () => {
-    const { webGlIns, vertsxShader, fragmentShader } = this.state 
-    initShaders(webGlIns, vertsxShader, fragmentShader)
-    webGlIns.drawArrays(webGlIns.POINTS, 0, 1)
+    const { gl, vertsxShader, fragmentShader } = this.state 
+    initShaders(gl, vertsxShader, fragmentShader)
+    // 获取顶点着色器位置信息
+    const a_position = gl.getAttribLocation(gl.program, 'a_position')
+    // 设置顶点着色器位置信息
+    gl.vertexAttrib3f(a_position, 0.0, 0.0, 0.0)
+    gl.drawArrays(gl.POINTS, 0, 1)
+  }
+  initCanvasClick = () => {
+    const { canvas, gl } = this.state
+    canvas.addEventListener('click', ({ clientX, clientY }: { clientX: number, clientY: number }) => {
+      console.log(clientX, clientY)
+      const { top, left, height, width } = canvas.getBoundingClientRect()
+      // 计算鼠标与canvas画布点相对位置
+      const [rX, rY] = [clientX - left, clientY - top]
+      // 计算gl的中心点
+      const [glCenterX, glCenterY] = [width/2, height/2]
+      // 根据鼠标位置计算gl位置
+      // gl单位和像素单位映射
+      const [gX, gY] = [(rX - glCenterX)/glCenterX, -(rY - glCenterY)/glCenterY]
+      // 获取顶点着色器位置信息
+      const a_position = gl.getAttribLocation(gl.program, 'a_position')
+      // 设置顶点着色器位置信息
+      gl.vertexAttrib2f(a_position, gX, gY)
+      gl.drawArrays(gl.POINTS, 0, 1)
+    })
   }
   componentDidMount() {
     this.initWebGl([
-      this.drawPoints, 
+      this.drawPoints,
       // this.chnageCanvasColor
+      this.initCanvasClick
     ])
   }
   render() {
-    return <canvas className="canvas" id="canvas">
-      哈哈哈
-    </canvas>
+    return <canvas className="canvas" id="canvas"></canvas>
   }
 }
 
